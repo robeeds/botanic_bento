4306902f7f416a2cfa33b68f9374b188ca23410d
robeeds <71895118+RobeeDS@users.noreply.github.com>
2024-03-07 13:40:58 -0800

    Theme setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,29 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  mode: "jit",
   theme: {
-    extend: {},
+    screens: {
+      xxs: "320px",
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1024px",
+      lg: "1200px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        cream: "#F4F0E5",
+        grey: "#E7EADA",
+        lime: "#9BCE72",
+        lightGreen: "#D9DFCE",
+      },
+      fontFamily: {
+        playfair: ["Playfair", "serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
