b1b868f8ec78da18e98beec22e75584e1ddedff7
robeeds <71895118+RobeeDS@users.noreply.github.com>
2024-03-08 12:23:30 -0800

    Added reusable styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { useState } from 'react'
 import { Navbar } from "./components"
-import reactLogo from './assets/react.svg'
-import logo from '/Logo.png'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
---
+++ src/constants/style.js
@@ -0,0 +1,16 @@
+const styles = {
+    boxWidth: "xl:max-w-[1920px] w-full",
+
+    title: "font-playfair font-semibold xxs:text-[40px] md:text-[60px]",
+    subtitle: "font-playfair font-semibold xxs:text-[24px] md:text-[40px]",
+    heading: "font-montserrat font-semibold xxs:text-[36px]",
+    paragraph: "font-montserrat font-regular xxs:text-[20px]",
+
+    paddingX: "sm:px-16 px-6",
+    paddingy: "sm:py-16 py-6",
+
+    marginX: "sm:mx-16 mx-6",
+    marginY: "sm:my-16 my-6",
+}
+
+export default styles;
