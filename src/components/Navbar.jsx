d85dc379406f2f8575766a1bd1622d4818f9ce12
robeeds <71895118+robeeds@users.noreply.github.com>
2024-03-10 22:35:43 -0700

    Added responsive padding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,8 +15,8 @@ const Navbar = () => {
 
         {/* Search Bar */}
         <div className='flex flex-row items-center bg-cream rounded-full'>
-          <input type="text" name="Search" placeholder='Search Catalog' className='bg-cream py-2 px-4 font-montserrat placeholder:text-darkGreen tracking-wide rounded-full w-[500px] h-[48px]' />
-          <img src={search} className='mx-3 w-[30px] h-[30px] hover:cursor-pointer'/>
+          <input type="text" name="Search" placeholder='Search Catalog' className='flex flex-1 bg-cream py-2 px-4 font-montserrat placeholder:text-darkGreen outline-none tracking-wide rounded-full w-[500px] h-[40px]' />
+          <img src={search} className='md:mr-2 w-[24px] h-[24px] hover:cursor-pointer'/>
         </div>
         
         
@@ -24,16 +24,16 @@ const Navbar = () => {
         
         <div className='flex flex-row justify-end items-center font-montserrat text-cream'>
 
-          <p className='px-2 hover:cursor-pointer'>about</p>
-          <p className='px-2 hover:cursor-pointer'>contact</p>
+          <p className='md:px-2 lg:px-4 hover:cursor-pointer'>about</p>
+          <p className='md:px-2 lg:px-4 hover:cursor-pointer'>contact</p>
 
           {/* Shopping Bag */}
-          <div className='flex bg-cream rounded-full w-[48px] h-[48px] justify-center items-center mx-2 hover:cursor-pointer'>
+          <div className='flex bg-cream rounded-full w-[48px] h-[48px] justify-center items-center sm:mx-2 lg:mx-4 hover:cursor-pointer'>
             <img src={bag} alt='shopping bag' className='flex flex-1 max-w-[28px] max-h-[28px]' />
           </div>
 
           {/* Account */}
-          <div className='flex bg-cream rounded-full w-[48px] h-[48px] justify-center items-center mx-2 hover:cursor-pointer'>
+          <div className='flex bg-cream rounded-full w-[48px] h-[48px] justify-center items-center sm:mx-2 lg:mx-4 hover:cursor-pointer'>
             <img src={account} alt='account' className='flex flex-1 max-w-[32px] max-h-[32px]' />
           </div>
           
