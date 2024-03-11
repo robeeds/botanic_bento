e4f5555bb5b9aaaeabb61d98acd3d5201931bbec
robeeds <71895118+RobeeDS@users.noreply.github.com>
2024-03-10 22:23:14 -0700

    Adjusted navbar styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
+    <div className={`${styles.boxWidth}`}>
 
-      <div className={`${styles.marginX} ${styles.paddingX}`}>
+      <div className={`${styles.paddingX} bg-radial_gradient`}>
         <Navbar />
       </div>
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { logo, search, bag, account } from '../assets'
+import styles from '../style'
 
 const Navbar = () => {
   return (
-    <div>
-      <div className='grid grid-cols-3 grid-flow-row items-center justify-between p-6'>
+    <div id="nav" className=''>
+      <div className={`grid grid-cols-3 grid-flow-row ${styles.paragraph} items-center justify-between p-6`}>
 
         {/* Brand */}
         <div className='flex flex-start items-center'>
@@ -13,26 +14,27 @@ const Navbar = () => {
         </div>
 
         {/* Search Bar */}
-        <div className='relative flex flex-row items-center'>
-          <input type="text" name="Search" placeholder='Search Catalog' className='bg-cream py-2 px-4 font-montserrat placeholder:text-lightGreen text-[24px] tracking-wide rounded-full w-[500px] h-[60px]' />
-          <img src={search} className='absolute right-3 w-[35px] h-[35px] hover:cursor-pointer'/>
+        <div className='flex flex-row items-center bg-cream rounded-full'>
+          <input type="text" name="Search" placeholder='Search Catalog' className='bg-cream py-2 px-4 font-montserrat placeholder:text-darkGreen tracking-wide rounded-full w-[500px] h-[48px]' />
+          <img src={search} className='mx-3 w-[30px] h-[30px] hover:cursor-pointer'/>
         </div>
         
         
         {/* Links */}
-        <div className='flex flex-row justify-end items-center font-montserrat text-[20px]'>
+        
+        <div className='flex flex-row justify-end items-center font-montserrat text-cream'>
 
           <p className='px-2 hover:cursor-pointer'>about</p>
           <p className='px-2 hover:cursor-pointer'>contact</p>
 
           {/* Shopping Bag */}
-          <div className='flex bg-cream rounded-full w-[60px] h-[60px] justify-center items-center mx-2 hover:cursor-pointer'>
-            <img src={bag} className='flex flex-1 max-w-[35px] max-h-[35px]' />
+          <div className='flex bg-cream rounded-full w-[48px] h-[48px] justify-center items-center mx-2 hover:cursor-pointer'>
+            <img src={bag} alt='shopping bag' className='flex flex-1 max-w-[28px] max-h-[28px]' />
           </div>
 
           {/* Account */}
-          <div className='flex bg-cream rounded-full w-[60px] h-[60px] justify-center items-center mx-2 hover:cursor-pointer'>
-            <img src={account} className='flex flex-1 max-w-[40px] max-h-[40px]' />
+          <div className='flex bg-cream rounded-full w-[48px] h-[48px] justify-center items-center mx-2 hover:cursor-pointer'>
+            <img src={account} alt='account' className='flex flex-1 max-w-[32px] max-h-[32px]' />
           </div>
           
         </div>
