c51bdc57c8bcc4f4790129ce95e7b5ea83fb7fdd
robeeds <71895118+RobeeDS@users.noreply.github.com>
2024-03-12 20:59:25 -0700

    Completed Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,47 +1,77 @@
-import React from 'react'
-import { logo, search, bag, account } from '../assets'
+import { useState } from 'react'
+import { logo, search, bag, account, menu, exit } from '../assets'
 import styles from '../style'
 
 const Navbar = () => {
+
+  {/* Setting Menu Toggle */}
+  const [toggle, setToggle] = useState(false);
+
   return (
-    <div id="nav" className=''>
-      <div className={`grid grid-cols-3 grid-flow-row ${styles.paragraph} items-center justify-between p-6`}>
+    <nav className={`grid grid-cols-3 grid-flow-row gap-2 p-6 items-center font-montserrat`}>
+      
+      {/* Brand */}
+      <div className='flex flex-row items-center xxs:col-span-2 md:col-span-1'>
+        <img src={logo} alt='Botanic Bento' className='h-[32px] w-[32px]'/>
+        <p className='font-playfair font-semibold xxs:text-[18px] sm:text-[32px] ml-2'>Botanic Bento</p>
+      </div>
 
-        {/* Brand */}
-        <div className='flex flex-start items-center'>
-          <img src={logo} alt='Botanic Bento' className='w-[32px] h-[32px]' />
-          <p className='text-[32px] px-2 font-playfair font-semibold'>Botanic Bento</p>
+      {/* MD Visible Nav */}
+      <div className='xxs:hidden md:flex col-span-2'>
+
+        {/* SearchBar */}
+        <div className='flex flex-1 items-center col-span-1 bg-cream rounded-full'>
+          <input placeholder='Search Catalog...' className='flex flex-1 placeholder-green text-green bg-cream py-2 px-4 outline-none rounded-full' />
+          <img src={search} alt='search' className='h-[32px] w-[32px] right-0 mx-2 hover:cursor-pointer'/>
         </div>
 
-        {/* Search Bar */}
-        <div className='flex flex-row items-center bg-cream rounded-full'>
-          <input type="text" name="Search" placeholder='Search Catalog' className='flex flex-1 bg-cream py-2 px-4 font-montserrat placeholder:text-darkGreen outline-none tracking-wide rounded-full w-[500px] h-[40px]' />
-          <img src={search} className='md:mr-2 w-[24px] h-[24px] hover:cursor-pointer'/>
-        </div>
-        
-        
         {/* Links */}
-        
-        <div className='flex flex-row justify-end items-center font-montserrat text-cream'>
-
-          <p className='md:px-2 lg:px-4 hover:cursor-pointer'>about</p>
-          <p className='md:px-2 lg:px-4 hover:cursor-pointer'>contact</p>
-
-          {/* Shopping Bag */}
-          <div className='flex bg-cream rounded-full w-[48px] h-[48px] justify-center items-center sm:mx-2 lg:mx-4 hover:cursor-pointer'>
-            <img src={bag} alt='shopping bag' className='flex flex-1 max-w-[28px] max-h-[28px]' />
-          </div>
-
-          {/* Account */}
-          <div className='flex bg-cream rounded-full w-[48px] h-[48px] justify-center items-center sm:mx-2 lg:mx-4 hover:cursor-pointer'>
-            <img src={account} alt='account' className='flex flex-1 max-w-[32px] max-h-[32px]' />
-          </div>
+        <div className='flex flex-1 flex-row col-span-1 items-center justify-end text-grey'>
           
+          <a className='px-2 hover:cursor-pointer' href='#about'>about</a>
+          <a className='px-2 hover:cursor-pointer' href='#contact'>contact</a>
+          <a  href='#bag' className='flex bg-cream h-[40px] w-[40px] rounded-full mx-2 px-2 items-center hover:cursor-pointer'>
+            <img src={bag} alt='shopping bag' className='h-[32px] w-[32px]' />
+          </a>
+          <a href='#account' className='flex bg-cream h-[40px] w-[40px] rounded-full mx-2 px-2 hover:cursor-pointer items-center'>
+            <img src={account} alt='account' className='h-[32px] w-[32px]' />
+          </a>
+          
+        </div>
+        
+      </div>
+
+      {/* SM Visible Menu */}
+      <div className='z-[5] flex justify-end items-center md:hidden'>
+
+        {/* Menu Toggle on Click */}
+        <img src={toggle ? exit : menu} 
+          alt="menu" 
+          className="w-[32px] h-[32px] object-contain" 
+          onClick={() => setToggle((prev) => !prev)}/>
+
+        {/* Menu List Items */}
+        <div className={`${toggle ? 'flex' : 'hidden'} flex flex-col absolute right-0 px-6 xxs:top-[80px] sm:top-[96px] bg-green rounded-bl-xl`}>
+          <div className='flex flex-1 flex-col text-grey'>
+
+            {/* SearchBar */}
+            <div className='flex flex-1 items-center col-span-1 bg-cream rounded-full mt-2'>
+              <input className='flex flex-1 h-[24px] text-green bg-cream p-2 outline-none rounded-full' />
+              <img src={search} alt='search' className='h-[24px] w-[24px] right-0 mx-2 my-1 hover:cursor-pointer'/>
+            </div>
+
+            {/* Links */}
+            <a className='pt-2 hover:cursor-pointer' href='#about'>about</a>
+            <a className='pt-2 hover:cursor-pointer' href='#contact'>contact</a>
+            <a className='pt-2 hover:cursor-pointer' href='#bag'>bag</a>
+            <a className='py-2 hover:cursor-pointer' href='#account'>account</a>
+
+          </div>
         </div>
 
       </div>
       
-    </div>
+    </nav>
   )
 }
 
