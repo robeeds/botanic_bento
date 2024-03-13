00c469f0aeb6bd76eb451638cee4aaf67e46a53c
robeeds <71895118+RobeeDS@users.noreply.github.com>
2024-03-13 11:07:52 -0700

    Completed Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,12 +6,14 @@ const Hero = () => {
   return (
     <div className={`relative ${styles.boxWidth} w-full`}>
         <section id="home" className='flex xxs:flex-wrap sm:flex-nowrap md:justify-center'>
-            <div className='flex flex-col sm:self-center ml-6'>
+            <div className='z-[4] flex flex-col sm:self-center ml-6'>
                 <p className={`${styles.title} whitespace-nowrap pb-0 mb-0`}>Botanic Bento</p>
                 <p className={`${styles.subtitle} whitespace-nowrap -mt-3 lg:-mt-10`}>Bring Beauty to your home</p>
-                <button className='flex bg-lime text-[24px] text-cream font-montserrat justify-center max-w-[258px] rounded-full py-2'>Explore Catalog</button>
+                <div className='flex'>
+                  <a href='#catalog' className='bg-lime font-montserrat text-grey px-2 md:px-4 py-1 md:py-2 rounded-full'>Explore Catalog</a>
+                </div>
             </div>
-            <div className='z-[4] self-center'>
+            <div className='z-[3] self-center sm:py-6 md:py-0'>
                 <img src={landingplant} alt='landing plant' className='w-[100%] h-[100%]'/>
             </div>
         </section>
