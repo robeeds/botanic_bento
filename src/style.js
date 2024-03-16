6f4da2250e7652c7c28432041bf1d50f0efae9df
robeeds <71895118+RobeeDS@users.noreply.github.com>
2024-03-16 12:35:24 -0700

    Added text responsiveness

--- src/style.js
+++ src/style.js
@@ -1,9 +1,9 @@
 const styles = {
     boxWidth: "xl:max-w-[1920px] w-full",
 
-    title: "font-playfair font-semibold xxs:text-[40px] md:text-[60px]",
-    subtitle: "font-playfair font-semibold xxs:text-[24px] md:text-[40px]",
-    heading: "font-montserrat font-semibold xxs:text-[36px]",
+    title: "font-playfair font-bold xxs:text-[36px] sm:text-[60px] lg:text-[96px]",
+    subtitle: "font-playfair font-semibold xxs:text-[20px] md:text-[44px] lg:text-[60px]",
+    heading: "font-montserrat font-semibold xxs:text-[20px] sm:text-[24px] md:text-[30px] lg:text-[36px]",
     paragraph: "font-montserrat font-regular xxs:text-[18px]",
 
     paddingX: "sm:px-16 px-6",
