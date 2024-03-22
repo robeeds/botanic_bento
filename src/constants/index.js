6f58a586fb834bf7fdd1091c4b1d9123f3eb05fa
robeeds <71895118+RobeeDS@users.noreply.github.com>
2024-03-21 21:12:34 -0700

    Navbar link imports

---
+++ src/constants/index.js
@@ -0,0 +1,22 @@
+import { bag, account } from "../assets"
+
+export const navLinks = [
+    {
+        id: "about",
+        link: "#about",
+    },
+    {
+        id: "contact",
+        link: "contact",
+    },
+    {
+        id: "bag",
+        link: "#bag",
+        img: {bag},
+    },
+    {
+        id: "account",
+        link: "#account",
+        img: {account},
+    }
+];
