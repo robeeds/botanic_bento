05eaa18f5bbf07a1d3434d6548ee959116f62f1d
robeeds <71895118+RobeeDS@users.noreply.github.com>
2024-03-27 13:04:13 -0700

    Add testimonial card information

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-import { bag, account } from "../assets"
+import { bag, account, logo, pfpone, pfptwo, pfpthree } from "../assets"
 
 export const navLinks = [
     {
@@ -12,11 +12,32 @@ export const navLinks = [
     {
         id: "bag",
         link: "#bag",
-        img: {bag},
+        img: bag,
     },
     {
         id: "account",
         link: "#account",
-        img: {account},
+        img: account,
     }
 ];
+
+export const testimonialInfo = [
+    {
+        id: "1",
+        quote: "Botanic Bento's plants have truly transformed my home! As a busy actress, I cherish the calming presence they bring to my space. It's like having a little piece of nature right inside my living room.",
+        image: pfpone,
+        name: "Jane Doe",
+    },
+    {
+        id: "2",
+        quote: "I'm constantly entertaining guests, and Botanic Bento's plants add that extra touch of elegance to my home. They not only look stunning but also create a welcoming atmosphere that everyone notices.",
+        image: pfptwo,
+        name: "John Doe",
+    },
+    {
+        id: "3",
+        quote: "I'm obsessed with Botanic Bento's plants! They've completely revitalized my living space, bringing in a breath of fresh air and injecting life into every room. I can't imagine my home without them now.",
+        image: pfpthree,
+        name: "Joe Doe",
+    },
+]
