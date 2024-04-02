0d450268556d45cad8a393f0e64aa5f99c782e77
robeeds <71895118+RobeeDS@users.noreply.github.com>
2024-04-01 21:08:05 -0700

    Changed subtitle styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
         <section id="home" className='flex xxs:flex-wrap sm:flex-nowrap md:justify-center'>
             <div className='z-[4] flex flex-col sm:self-center ml-6'>
                 <p className={`${styles.title} whitespace-nowrap pb-0 mb-0`}>Botanic Bento</p>
-                <p className={`${styles.subtitle} whitespace-nowrap -mt-3 lg:-mt-10`}>Bring Beauty to your home</p>
+                <p className={`font-playfair font-semibold xxs:text-[20px] md:text-[44px] lg:text-[60px] whitespace-nowrap -mt-3 lg:-mt-10`}>Bring Beauty to your home</p>
                 <div className='flex'>
                   <a href='#catalog' className='bg-lime font-montserrat text-white px-2 md:px-4 py-1 md:py-2 rounded-full'>Explore Catalog</a>
                 </div>
