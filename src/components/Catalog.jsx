c180d318fe9c505f1a4b12672a18fa123fd422e4
robeeds <71895118+RobeeDS@users.noreply.github.com>
2024-04-11 12:24:08 -0700

    Complete Catalog Section

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -1,68 +1,66 @@
 import React from 'react'
-import { card, blossoming, open } from '../assets'
+import { card, blossoming, open, rectanglecard, catalogtwo, catalogthree, catalogfour, catalogfive } from '../assets'
 import styles from '../style'
 
 const Catalog = () => {
   return (
     <div className={`bg__green p-6 w-full ${styles.paddingX} ${styles.paddingY}`}>
       <section id='catalog' className='px-6'>
-        <p className={`${styles.subtitle} text-center`}>Our Latest Releases</p>
+        <p className={`${styles.subtitle} text-center pb-6`}>Our Latest Releases</p>
         
         {/* Bento Section */}
-        <div className='grid grid-cols-6 grid-rows-3 gap-x-6 gap-y-6'>
+        <div className='grid grid-flow-row gap-4 sm:grid-cols-6 justify-center max-w-[1368px] md:max-h-[732px]'>
 
-          {/* Custom Card: Spring Catalog */}
-          <div className='relative flex flex-col col-span-3 row-span-2'>
 
-            {/* SVG for background of Card */}
-            <img src={card} alt='Custom Card'/>
+            {/* MD First Row */}
+            <div className='relative flex w-full sm:col-span-3'>
+              <img src={card} alt='Custom Card' className=''/>
+              <div className='absolute w-[100%] h-[100%]'>
 
-            <div className='absolute flex flex-col w-[100%] h-[100%]'>
-
-              {/* Top Div */}
-              <div className='w-[100%] h-[68%] flex justify-center p-8'>
-
-                {/* Image */}
-                <img src={blossoming} alt='Green Living Rooom' className='flex self-center object-cover w-[100%] h-[100%] rounded-t-[30px] rounded-bl-[20px] rounded-br-[25px]'/>
-              </div>
-
-              {/* Bottom Div */}
-              <div className='w-[100%] h-[32%] flex flex-row justify-between items-center'>
-
-                {/* Text */}
-                <div className='w-[79%] flex flex-col items-center'>
-                  <p className='font-montserrat font-medium xxs:text-[24px] lg:text-[42px]'>Blossoming Beauties</p>
-                  <p className='font-montserrat xxs:text-[12px] lg:text-[18px]'>Welcome the Season with our Spring Collection</p>
+                {/* Image Section */}
+                <div className='flex items-center justify-center w-[100%] h-[67%]'>
+                  <img src={blossoming} alt='Spring Catalog' className='w-[93%] h-[85%] rounded-xl lg:rounded-t-[30px] lg:rounded-bl-[20px] lg:rounded-br-[25px]' />
                 </div>
 
-                {/* Button */}
-                <div className='w-[18%] h-[85%] flex self-end justify-center'>
-                  <a href='#spring' className='self-center'>
-                    <img src={open} className='cursor-pointer self-center w-[50px] hover:w-[60px] h-[50px] hover:h-[60px] duration-300' />
-                  </a>
-                </div>
-              </div>
+                <div className='flex flex-row h-[33%]'>
 
+                  {/* Title */}
+                  <div className='flex flex-1 flex-col items-center justify-center w-[79%] font-montserrat text-center'>
+                    <p className='font-medium text-[14.5px] xs:text-[18px] sm:text-[20px] md:text-[28px] lg:text-[42px]'>Blossoming Beauties</p>
+                    <p className='text-[8px] md:text-[12px] lg:text-[18px]'>Welcome the Season with Our Spring Collection</p>
+                  </div>
+
+                  {/* Button */}
+                  <div className='flex justify-self-end self-end w-[18%] h-[84%] justify-center items-center'>
+                    <img src={open} alt='' className='w-[50%] h-[50%] hover:w-[60%] hover:h-[60%] duration-100 cursor-pointer' />
+                  </div>
+
+                </div>
+
+              </div>
             </div>
-     
-          </div>
 
-          {/* Catalog 2 */}
-          <div className='bg-cream col-span-3 row-span-2 rounded-[40px] flex justify-center items-center p-7'>
-            <img src={blossoming} alt='Catalog #2 Preview' className='object-cover w-[100%] h-[100%] rounded-[30px]'/>
-          </div>
+            {/* Catalog Two */}
+            <div className='relative flex w-full sm:col-span-3 justify-center items-center'>
+              <img src={rectanglecard} alt='Card' className='z-[1]'/>
+              <img src={catalogtwo} alt='Catalog Two' className='absolute z-[2] w-[93%] h-[89%] rounded-xl lg:rounded-[30px]' />
+            </div>
 
-          <div className='bg-cream col-span-2 row-span-1 rounded-[30px]'>
+            {/* MD Second Row */}
+            <div className='relative flex w-full sm:col-span-2 justify-center items-center'>
+              <img src={rectanglecard} alt='Card' className='z-[1]'/>
+              <img src={catalogthree} alt='Catalog Three' className='absolute z-[2] w-[93%] h-[89%] rounded-xl lg:rounded-[20px]' />
+            </div>
 
-          </div>
+            <div className='relative flex w-full sm:col-span-2 justify-center items-center'>
+              <img src={rectanglecard} alt='Card' className='z-[1]'/>
+              <img src={catalogfour} alt='Catalog Four' className='absolute z-[2] w-[93%] h-[89%] rounded-xl lg:rounded-[20px]' />
+            </div>
 
-          <div className='bg-cream col-span-2 row-span-1 rounded-[30px]'>
-
-          </div>
-
-          <div className='bg-cream col-span-2 row-span-1 rounded-[30px]'>
-
-          </div>
+            <div className='relative flex w-full sm:col-span-2 justify-center items-center'>
+              <img src={rectanglecard} alt='Card' className='z-[1]'/>
+              <img src={catalogfive} alt='Catalog Five' className='absolute z-[2] w-[93%] h-[89%] rounded-xl lg:rounded-[20px]' />
+            </div>
 
         </div>
 
